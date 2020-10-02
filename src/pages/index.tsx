7b171e35bed4d782b47a4a73269ed561bb702a67
Alex Madeira <alex.c.madeira@gmail.com>
2020-10-02 19:16:12 -0300

    feat: add swr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,23 @@ import Head from 'next/head'
 
 import Logo from '../assets/1.svg'
 import { Container, LogoBox } from '~/styles/pages/Home'
+import { useFetch } from '~/hooks/useFetch'
+
+interface Todo {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
 
 const Home: React.FC = () => {
+  const { data } = useFetch<Todo[]>('todos')
+  if (!data) {
+    return null
+  }
+
+  console.log(data[0].id)
+
   return (
     <Container>
       <Head>
